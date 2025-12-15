081c8a6e8b037478014a0174300a13a6c45acdcc
Dale Hunt <dalehunt@gmail.com>
2025-12-15 17:01:52 +0000

    taint fixes

--- Modules/CooldownManager.lua
+++ Modules/CooldownManager.lua
@@ -251,7 +251,6 @@ function BCDM:UpdateCooldownViewer(cooldownViewer)
     AdjustChargeCount(cooldownViewer)
     ApplyCooldownText(cooldownViewer)
     AdjustCooldownManagerStrata()
-    if _G[cooldownViewer] and _G[cooldownViewer].Layout  and not InCombatLockdown() then _G[cooldownViewer]:Layout() end
     PositionCooldownViewers()
     BCDM:SetPowerBarWidth()
     BCDM:SetSecondaryPowerBarWidth()
